290c87ad244b729cc06ad890c60d6905ea83ef73
reysl-04 <alex.reyesl0410@gmail.com>
2026-05-02 18:10:30 -0400

    Add CORS

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-import express from "express";
+import express from "express"
+import cors from "cors"
+
 import listsRouter from "./routes/lists.js"
 import tasksRouter from "./routes/tasks.js"
+import errorHandler from "./middlewares/errorHandler.js"
 
-import errorHandler from "./middlewares/errorHandler.js";
+const corsOptions = {
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+    maxAge: 84500
+}
 
 const app = express();
 
+app.use(cors(corsOptions))
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}))
 
 app.use('/lists', listsRouter)
 app.use('/tasks', tasksRouter)
