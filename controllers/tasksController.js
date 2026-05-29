faf4d6faaee850ec239bc5b4c2c736edb37d13ce
reysl-04 <alex.reyesl0410@gmail.com>
2026-05-28 19:25:32 -0500

    Update tasks controller

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,13 +1,15 @@
-import store from "../data/store.js"
+import pool from "../src/db/pool.js"
+
 import crypto from "node:crypto"
 import { NotFoundError, ValidationError } from "../errors/customErrors.js"
+import { error } from "node:console"
 
 
-export function getAllTasks(req, res, next) {
-    const listId = req.params.listId
+export async function getAllTasks(req, res, next) {
+    const userId = req.params.userId
     const filterKeys = Object.keys(req.query)
 
-    const allowedFilters = ['status', 'dueDate', 'name']
+    const allowedFilters = ['status', 'due_date']
     
     for (const key of filterKeys) {
         if (!allowedFilters.includes(key)) {
@@ -15,86 +17,101 @@ export function getAllTasks(req, res, next) {
         }
     }
 
-    const allTasks = store.tasks.filter((task) => {
-        if (task.listId !== listId) {
-            return false
-        }
-        for (const key of filterKeys) {
-            if (req.query[key] !== task[key]) {
-                return false
-            }
-        }
-        return true
-    })
+    const queryValues = []
+    const whereClauses = []
 
-    res.status(200).json(allTasks)
+    for (const key of filterKeys) {
+        queryValues.push(req.query[key])
+        whereClauses.push(`${key} = $${queryValues.length}`)
+    }
+    queryValues.push(userId)
+    whereClauses.push(`user_id = $${queryValues.length}`)
+
+    const query = `SELECT * FROM tasks WHERE ` + whereClauses.join(` AND `)
+
+    const { rows } = await pool.query(query, queryValues)
+    res.status(200).json(rows)
 }
 
-export function getTask(req, res, next) {
-    const taskId = req.params.taskId // Validate Id? I don't t
-    const taskIndex = store.tasks.findIndex(task => task.id === taskId)
-    
-    if (taskIndex !== -1) {
-        res.status(200).json(store.tasks[taskIndex])
+export async function getTask(req, res, next) {
+    const taskId = req.params.taskId
+
+    const { rows } = await pool.query(`
+            SELECT *
+            FROM tasks
+            WHERE id = $1
+        `, [taskId])
+
+    if (!rows[0]) {
+        return next(new NotFoundError(`Task Id not found: ${taskId}`))
     } else {
-        return next(new NotFoundError(`Task ID not found: ${taskId}`))
+        res.status(200).json(rows[0])
     }
 }
 
-export function postTask(req, res, next) {
-    // Requires listId, dueDate, name & description
-    const taskBody = req.body
-    const validListId = store.lists.some(list => list.id === req.params.listId)
-    
-    if (!validListId) {
-        return next(new NotFoundError(`Invalid List ID: ${taskBody.listId}`))
-    }
-
-    // I have to validate due date and the other fields, but i think I can use Zod for that. Ill do it later
-    const newTask = {
-        listId: req.params.listId,
-        id: crypto.randomUUID(),
-        createdAt: new Date().toISOString(),
-        dueDate: taskBody.dueDate,
-        status: 'pending',
-        name: taskBody.name,
-        description: taskBody.description
-    }
-
-    store.tasks.push(newTask)
-    res.status(201).json({ message: `New task created - ID: ${newTask.id}` }) // Should I json the whole task information?
-
-}
-
-export function updateTask(req, res, next) {
-    const taskId = req.params.taskId
-    const taskIndex = store.tasks.findIndex(task => task.id === taskId)
-    
-    if (taskIndex === -1) {
-        return next(new NotFoundError(`Task ID not found: ${taskId}`))
-    }
-
+export async function postTask(req, res, next) {
+    const userId = req.params.userId
     const taskBody = req.body
 
-    for (const field in taskBody) {
-        store.tasks[taskIndex][field] = taskBody[field]
+    try {
+        const { rows } = await pool.query(`
+            INSERT INTO tasks (user_id, title, description, due_date, status)
+            VALUES ($1, $2, $3, $4, 'pending')
+            RETURNING id
+        `, [userId, taskBody.title, taskBody.description, taskBody.dueDate])
+
+        res.status(201).json({ message: `New task created. ID: ${rows[0].id}` })
+    } catch(e) {
+        if (e.code === '23503') {
+            return next(new NotFoundError(`User not found`))
+        }
+        return next(e)
     }
-     
-    res.status(200).json({ message: `Task Updated - ID: ${taskId} `})
 }
 
-export function deleteTask(req, res, next) {
+export async function updateTask(req, res, next) {
     const taskId = req.params.taskId
-    const taskIndex = store.tasks.findIndex(task => task.id === taskId)
+    const allowedFields = ['title', 'description', 'due_date', 'status']
 
-    if (taskIndex === -1) {
-        return next(new NotFoundError(`Task ID not found: ${taskId}`))
+    const updates = Object.keys(req.body).filter(key => allowedFields.includes(key))
+    if (updates.length === 0) {
+        return next(new ValidationError(`No valid field provided`))
     }
 
-    store.tasks.splice(taskIndex, 1)
-    res.status(204).send()
+    const setClause = updates.map((key, i) => {
+        return `${key} = $${i + 1}`
+    })
+    const values = updates.map(key => req.body[key])
+    values.push(taskId)
+
+    const query = `UPDATE tasks SET ${setClause.join(", ")} WHERE id = $${values.length} RETURNING id`
+    try {
+        const { rows } = await pool.query(`${query}`, values)
+        if (!rows[0]) {
+            return next(new NotFoundError(`Task Id not found`))
+        } else {
+            res.status(200).json({message: `Task updated: ${taskId}`})
+        }
+    } catch(e) {
+        return next(e)
+    }
+}
 
 
+export async function deleteTask(req, res, next) {
+    const taskId = req.params.taskId
+
+    const { rows } = await pool.query(`
+        DELETE FROM tasks
+        WHERE id = $1
+        RETURNING id
+    `, [taskId])
+
+    if (!rows[0]) {
+        return next(new NotFoundError(`Task ID not found: ${taskId}`))
+    } else {
+        res.status(204).send()
+    }
 }
 
 // task Id and task index are really repetitive, so Ill have a function thast is called when that work need to be done
