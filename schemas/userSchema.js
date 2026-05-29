15d13b5925bb031e556106618f873d0a1960cb0c
reysl-04 <alex.reyesl0410@gmail.com>
2026-05-28 21:22:34 -0500

    Add users schema

---
+++ schemas/userSchema.js
@@ -0,0 +1,18 @@
+import { z } from "zod"
+
+export const createUserSchema = z.object({
+    name: z.string().min(1).max(25),
+    email: z.email().endsWith('@gmail.com'),
+    password: z.string().min(8).max(72)
+})
+.strict()
+
+export const updateUserSchema = z.object({
+    name: z.string.min(1).max(25),
+    email: z.email().endsWith('@gmail.com'),
+})
+.strict()
+.partial()
+.refine(data => Object.keys(data).length > 0, {
+    message: "At least one field is required"
+})
